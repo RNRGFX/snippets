ef9d9094ce9f80b078952f4a61156beeaed68867
RNRGFX <daniel@da9l.dk>
2025-05-14 10:44:14 +0200

    Create wpml-remove-translate-independantly.php

---
+++ wpml-remove-translate-independantly.php
@@ -0,0 +1,19 @@
+
+<?php 
+
+add_action('save_post', 'wpml_flag_post_for_independence', 99, 2);
+
+// Auto remove WPML duplicate post flag
+add_action('save_post', 'wpml_flag_post_for_independence', 99, 2);
+
+function wpml_flag_post_for_independence($post_id, $post) {
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
+    
+    // Delay removal until after everything else has run
+    add_action('shutdown', function() use ($post_id) {
+        $duplicate_of = get_post_meta($post_id, '_icl_lang_duplicate_of', true);
+        if (!empty($duplicate_of)) {
+            delete_post_meta($post_id, '_icl_lang_duplicate_of');
+        }
+    });
+}
