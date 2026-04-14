a37ac4cc7e95b8ba8d008cc18c140b4829d5c5fd
RNRGFX <daniel@da9l.dk>
2026-04-14 12:18:35 +0200

    Rename network from 'rnrgfx' to 'YOUR_NETWORK'

--- Docker Templates/setup_network.sh
+++ Docker Templates/setup_network.sh
@@ -2,4 +2,4 @@
 docker network inspect $(docker network ls -q) | grep "Subnet\|Gateway"
 
 # Create network with an IP-address NOT in use by above results:
-docker network create --driver=bridge --subnet=172.20.0.0/16 --gateway=172.20.0.1 rnrgfx
+docker network create --driver=bridge --subnet=172.20.0.0/16 --gateway=172.20.0.1 YOUR_NETWORK
